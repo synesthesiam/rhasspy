31dbf765aaf394c2277c9bda89d87d13484afcaf
Michael Hansen <hansen.mike@gmail.com>
2018-12-02 15:18:12 -0500

    Successful remote speech to text test

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-docker run --rm -it -p 12101:12101 \
+
+# Usually x86_64 or armhf
+CPU_ARCH="$(lscpu | awk '/^Architecture/{print $2}')"
+
+# Use amd64 instead of x86_64 for consistency with home assistant's BUILD_ARCH
+case $CPU_ARCH in
+    x86_64)
+        CPU_ARCH=amd64
+esac
+
+docker run -d -p 12101:12101 \
        -v $(pwd):/rhasspy \
        -v /dev/snd:/dev/snd \
        --privileged \
-       rhasspy-hassio:0.1-alpha
+       synesthesiam/rhasspy-hassio-addon:${CPU_ARCH}
