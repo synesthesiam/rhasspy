857cf2aaf88fa449ded0562ac7aad1b7e8abe231
Michael Hansen <hansen.mike@gmail.com>
2018-12-03 21:52:48 -0500

    Working on profile settings tab

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -3,5 +3,9 @@ import Api from '@/services/Api'
 export default {
     getProfiles() {
         return Api().get('/api/profiles')
+    },
+
+    getProfileSettings(profile) {
+        return Api().get('/api/profile', { params: { 'profile': profile } })
     }
 }
