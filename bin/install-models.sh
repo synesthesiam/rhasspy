16f8257a53ee3df7005f93c1d1fc7d4a9930826a
Michael Hansen <hansen.mike@gmail.com>
2018-12-07 01:16:23 +0000

    Successful build of server Docker image

--- bin/install-models.sh
+++ bin/install-models.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 for lang in "$@"; do
-    python3 -m spacy install $lang
+    python3 -m spacy download $lang
 done
---
+++ run-server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+LANGUAGE=en
+
+# Usually x86_64 or armhf
+CPU_ARCH="$(lscpu | awk '/^Architecture/{print $2}')"
+
+# Use amd64 instead of x86_64 for consistency with home assistant's BUILD_ARCH
+case $CPU_ARCH in
+    x86_64)
+        CPU_ARCH=amd64
+esac
+
+docker run --rm -it --name rhasspy-server \
+       -p 12101:12101 \
+       --device /dev/snd:/dev/snd \
+       --privileged \
+       synesthesiam/rhasspy-server:${CPU_ARCH}-$LANGUAGE
