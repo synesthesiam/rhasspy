synesthesiam/rhasspy

commit 9d062a0fa2e6b8ff050a71aadc618036baa7d63c

Next attempt at profile download

profiles/ru/download-profile.sh
@@ -15,14 +15,14 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-ru-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
 
 echo "Extracting acoustic model (${acoustic_file})"
 rm -rf "${acoustic_output}"
-tar -xzf "${acoustic_file}" && mv "${DIR}/cmusphinx-ru-5.2" "${acoustic_output}" && rm -f "${acoustic_output}/ru.dic" "${acoustic_output}/rm.lm"
+tar -xzf "${acoustic_file}" && mv "${DIR}/cmusphinx-ru-5.2" "${acoustic_output}" && rm -s "${acoustic_output}/ru.dic" "${acoustic_output}/rm.lm"
 
 #------------------------------------------------------------------------------
 # G2P
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/ru-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -63,7 +63,7 @@ mv "${acoustic_output}/ru.lm" "${lm_output}"
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/de/download-profile.sh
@@ -15,7 +15,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-de-voxforge-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/de-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -56,7 +56,7 @@ lm_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0-
 lm_file="${download_dir}/cmusphinx-voxforge-de.lm.gz"
 lm_output="${DIR}/base_language_model.txt"
 
-if [[ ! -f "${lm_file}" ]]; then
+if [[ ! -s "${lm_file}" ]]; then
     echo "Downloading language model"
     wget -q -O "${lm_file}" "${lm_url}"
 fi
@@ -71,7 +71,7 @@ zcat "${lm_file}" > "${lm_output}" || exit 1
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/el/download-profile.sh
@@ -16,7 +16,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-el-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -33,7 +33,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/el-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -64,7 +64,7 @@ tar --to-stdout -xzf "${acoustic_file}" 'cmusphinx-el-gr-5.2/el-gr.lm.gz' | zcat
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/en/download-profile.sh
@@ -3,6 +3,11 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 download_dir="${DIR}/download"
+
+if [[ "$1" = "--delete" ]]; then
+    rm -rf "${download_dir}"
+fi
+
 mkdir -p "${download_dir}"
 
 echo "Downloading English (en) profile (sphinx)"
@@ -17,7 +22,7 @@ acoustic_file="${download_dir}/cmusphinx-en-us-5.2.tar.gz"
 echo "${acoustic_file}"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -34,7 +39,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/en-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -59,7 +64,7 @@ lm_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0-
 lm_file="${download_dir}/en-70k-0.2-pruned.lm.gz"
 lm_output="${DIR}/base_language_model.txt"
 
-if [[ ! -f "${lm_file}" ]]; then
+if [[ ! -s "${lm_file}" ]]; then
     echo "Downloading language model"
     wget -q -O "${lm_file}" "${lm_url}"
 fi
@@ -74,7 +79,7 @@ zcat "${lm_file}" > "${lm_output}" || exit 1
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/es/download-profile.sh
@@ -15,7 +15,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-es-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/es-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -63,7 +63,7 @@ tar --to-stdout -xf "${acoustic_file}" "cmusphinx-es-5.2/etc/es-20k.lm.gz" | zca
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy.umdl[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         wget -q -O "${model_output}" "https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
     fi
 done
@@ -75,7 +75,7 @@ done
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/fr/download-profile.sh
@@ -15,7 +15,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-fr-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/fr-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -56,7 +56,7 @@ lm_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0-
 lm_file="${download_dir}/fr-small.lm.gz"
 lm_output="${DIR}/base_language_model.txt"
 
-if [[ ! -f "${lm_file}" ]]; then
+if [[ ! -s "${lm_file}" ]]; then
     echo "Downloading language model"
     wget -q -O "${lm_file}" "${lm_url}"
 fi
@@ -71,7 +71,7 @@ zcat "${lm_file}" > "${lm_output}" || exit 1
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/hi/download-profile.sh
@@ -15,7 +15,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-hi-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/hi-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -63,7 +63,7 @@ tar --to-stdout -xzf "${acoustic_file}" 'cmusphinx-hi-5.2/hindi.lm' > "${lm_outp
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

profiles/it/download-profile.sh
@@ -15,7 +15,7 @@ acoustic_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download
 acoustic_file="${download_dir}/cmusphinx-it-5.2.tar.gz"
 acoustic_output="${DIR}/acoustic_model"
 
-if [[ ! -f "${acoustic_file}" ]]; then
+if [[ ! -s "${acoustic_file}" ]]; then
     echo "Downloading acoustic model"
     wget -q -O "${acoustic_file}" "${acoustic_url}"
 fi
@@ -32,7 +32,7 @@ g2p_url='https://github.com/synesthesiam/rhasspy-profiles/releases/download/v1.0
 g2p_file="${download_dir}/it-g2p.tar.gz"
 g2p_output="${DIR}/g2p.fst"
 
-if [[ ! -f "${g2p_file}" ]]; then
+if [[ ! -s "${g2p_file}" ]]; then
     echo "Downloading g2p model"
     wget -q -O "${g2p_file}" "${g2p_url}"
 fi
@@ -64,7 +64,7 @@ tar --to-stdout -xzf "${acoustic_file}" 'cmusphinx-it-5.2/etc/voxforge_it_sphinx
 snowboy_models=("snowboy.umdl" "computer.umdl")
 for model_name in "${snowboy_models[@]}"; do
     model_output="${DIR}/${model_name}"
-    if [[ ! -f "${model_output}" ]]; then
+    if [[ ! -s "${model_output}" ]]; then
         model_url="https://github.com/Kitt-AI/snowboy/raw/master/resources/models/${model_name}"
         echo "Downloading ${model_output} (${model_url})"
         wget -q -O "${model_output}" "${model_url}"

commit 3d2dfd7ab61f2e9a7ae5a52fce70b9a5e7b8407e

Fix Russian profile
Fixes regression introduced in 9d062a0fa2e6b8ff050a71aadc618036baa7d63c

profiles/ru/download-profile.sh
@@ -32,7 +32,7 @@ fi
 
 echo "Extracting acoustic model (${acoustic_file})"
 rm -rf "${acoustic_output}"
-tar -C "${DIR}" -xzf "${acoustic_file}" && mv "${DIR}/cmusphinx-ru-5.2" "${acoustic_output}" && rm -s "${acoustic_output}/ru.dic" "${acoustic_output}/rm.lm"
+tar -C "${DIR}" -xzf "${acoustic_file}" && mv "${DIR}/cmusphinx-ru-5.2" "${acoustic_output}" && rm -f "${acoustic_output}/ru.dic" "${acoustic_output}/rm.lm"
 
 #------------------------------------------------------------------------------
 # G2P