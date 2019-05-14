33778d97a984cc426058c5e8281d9693f5887d7f
Michael Hansen <michael.hansen.24@us.af.mil>
2019-05-14 11:31:51 -0400

    English test is fully externalized

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 from rhasspy.core import RhasspyCore
 
+
 class RhasspyTestCore:
     def __init__(self, profile_name, train=True):
         self.profile_name = profile_name
@@ -26,7 +27,6 @@ class RhasspyTestCore:
         self.core.start(preload=False)
         self.core.train()
 
-
         if self.train:
             self.core.train()
 
@@ -39,90 +39,128 @@ class RhasspyTestCore:
         except:
             pass
 
+
 # -----------------------------------------------------------------------------
 
-PROFILES = ["en", "de"]
+PROFILES = ["en"]
+TEST_WAV_PATH = {p: os.path.join("etc", "test", p, "test.wav") for p in PROFILES}
+TEST_JSON = {
+    p: json.load(open(os.path.join("etc", "test", p, "test.json"))) for p in PROFILES
+}
+
 
 class RhasspyTestCase(unittest.TestCase):
-
     def test_transcribe(self):
         """speech -> text"""
         for profile_name in PROFILES:
             with RhasspyTestCore(profile_name) as core:
-                test_dir = os.path.join("etc", "test", profile_name)
-                wav_path = os.path.join(test_dir, "test.wav")
-                json_path = os.path.join(test_dir, "test.json")
-
-                with open(json_path, "r") as json_file:
-                    wav_info = json.load(json_file)
+                wav_path = TEST_WAV_PATH[profile_name]
+                test_info = TEST_JSON[profile_name]
 
                 with open(wav_path, "rb") as wav_file:
                     text = core.transcribe_wav(wav_file.read()).text
-                    self.assertEqual(text, wav_info["text"])
+                    self.assertEqual(text, test_info["text"])
 
     # -------------------------------------------------------------------------
 
-    # def test_recognize(self):
-    #     """text -> intent"""
-    #     intent = self.core.recognize_intent("turn on the living room lamp").intent
-    #     self.assertEqual(intent["intent"]["name"], "ChangeLightState")
-    #     entities = {e["entity"]: e["value"] for e in intent["entities"]}
-    #     self.assertEqual(entities["name"], "living room lamp")
-    #     self.assertEqual(entities["state"], "on")
+    def test_recognize(self):
+        """text -> intent"""
+        for profile_name in PROFILES:
+            with RhasspyTestCore(profile_name) as core:
+                test_info = TEST_JSON[profile_name]
+                intent = core.recognize_intent(test_info["text"]).intent
+                self.assertEqual(intent["intent"]["name"], test_info["intent"]["name"])
+
+                expected_entities = test_info["entities"]
+                for ev in intent["entities"]:
+                    entity = ev["entity"]
+                    if (entity in expected_entities) and (
+                        ev["value"] == expected_entities[entity]
+                    ):
+                        expected_entities.pop(entity)
+
+                self.assertEqual(expected_entities, {})
 
     # -------------------------------------------------------------------------
 
-    # def test_training(self):
-    #     """Test training"""
-    #     profile_name = "en"
-    #     with tempfile.TemporaryDirectory(prefix="rhasspy_") as temp_user_dir:
-    #         core = RhasspyCore(
-    #             profile_name, self.system_profiles_dir, temp_user_dir, do_logging=True
-    #         )
-    #         core.profile.set("rhasspy.listen_on_start", False)
-    #         core.profile.set("rhasspy.preload_profile", False)
-    #         core.start()
+    def test_training(self):
+        """Test training"""
+        for profile_name in PROFILES:
+            with RhasspyTestCore(profile_name, train=False) as core:
+                wav_path = TEST_WAV_PATH[profile_name]
+                test_info = TEST_JSON[profile_name]
 
-    #         sentences_path = core.profile.write_path(
-    #             core.profile.get("speech_to_text.sentences_ini")
-    #         )
+                old_sentences_path = core.profile.read_path(
+                    core.profile.get("speech_to_text.sentences_ini")
+                )
 
-    #         with open(sentences_path, "w") as sentences_file:
-    #             print("[Foo]", file=sentences_file)
-    #             print("foo bar", file=sentences_file)
-    #             print("foo bar baz", file=sentences_file)
+                new_sentences_path = core.profile.write_path(
+                    core.profile.get("speech_to_text.sentences_ini")
+                )
 
-    #         core.train()
-    #         with open("etc/test/what_time_is_it.wav", "rb") as wav_file:
-    #             text = core.transcribe_wav(wav_file.read()).text
-    #             self.assertNotEqual(text, "what time is it")
+                # Remove intent
+                expected_intent = test_info["intent"]["name"]
+                with open(old_sentences_path, "r") as old_sentences_file:
+                    with open(new_sentences_path, "w") as new_sentences_file:
+                        in_intent = False
+                        for line in old_sentences_file:
+                            line = line.strip()
 
-    #         # Add some more sentences
-    #         with open(sentences_path, "a") as sentences_file:
-    #             print("", file=sentences_file)
-    #             print("[GetTime]", file=sentences_file)
-    #             print("what time is it", file=sentences_file)
+                            if not in_intent and (line == f"[{expected_intent}]"):
+                                in_intent = True
+                            elif in_intent and line.startswith("["):
+                                # On to next intent
+                                in_intent = False
 
-    #         core.train()
-    #         with open("etc/test/what_time_is_it.wav", "rb") as wav_file:
-    #             text = core.transcribe_wav(wav_file.read()).text
-    #             self.assertEqual(text, "what time is it")
+                            if not in_intent:
+                                print(line, file=new_sentences_file)
+
+                # Train without target intent
+                core.train()
+                expected_text = test_info["text"]
+                with open(wav_path, "rb") as wav_file:
+                    text = core.transcribe_wav(wav_file.read()).text
+
+                    # Should fail to match
+                    self.assertNotEqual(text, expected_text)
+
+                # Delete changes
+                os.unlink(new_sentences_path)
+
+                core.train()
+                with open(wav_path, "rb") as wav_file:
+                    text = core.transcribe_wav(wav_file.read()).text
+
+                    # Should match now
+                    self.assertEqual(text, expected_text)
 
     # -------------------------------------------------------------------------
 
-    # def test_pronounce(self):
-    #     # Known word
-    #     pronunciations = self.core.get_word_pronunciations(["test"], n=1).pronunciations
-    #     self.assertEqual(pronunciations["test"]["pronunciations"][0], "T EH S T")
+    def test_pronounce(self):
+        for profile_name in PROFILES:
+            with RhasspyTestCore(profile_name) as core:
+                test_info = TEST_JSON[profile_name]
 
-    #     # Unknown word
-    #     pronunciations = self.core.get_word_pronunciations(
-    #         ["raxacoricofallipatorius"], n=1
-    #     ).pronunciations
-    #     self.assertIn(
-    #         "R AE K S AH K AO R IH K AO F AE L AH P AH T AO R IY IH S",
-    #         pronunciations["raxacoricofallipatorius"]["pronunciations"],
-    #     )
+                # Known word
+                known_word = test_info["words"]["known"]["word"]
+                pronunciations = core.get_word_pronunciations(
+                    [known_word]
+                ).pronunciations
+                self.assertIn(
+                    test_info["words"]["known"]["phonemes"],
+                    pronunciations[known_word]["pronunciations"],
+                )
+
+                # Unknown word
+                known_word = test_info["words"]["known"]["word"]
+                unknown_word = test_info["words"]["unknown"]["word"]
+                pronunciations = core.get_word_pronunciations(
+                    [unknown_word], n=5
+                ).pronunciations
+                self.assertIn(
+                    test_info["words"]["unknown"]["phonemes"],
+                    pronunciations[unknown_word]["pronunciations"],
+                )
 
 
 # -----------------------------------------------------------------------------
