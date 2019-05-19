bc81ce73499c482fba4d1e85b9081a195753288a
Michael Hansen <hansen.mike@gmail.com>
2019-05-18 20:50:56 -0400

    Fix French profile download

--- profiles/fr/download-profile.sh
+++ profiles/fr/download-profile.sh
@@ -32,7 +32,7 @@ fi
 
 echo "Extracting acoustic model (${acoustic_file})"
 rm -rf "${acoustic_output}"
-tar -C "${DIR}" -xf "${acoustic_file}" -C "${DIR}" && mv "${DIR}/cmusphinx-fr-5.2" "${acoustic_output}"
+tar -C "${DIR}" -xf "${acoustic_file}" && mv "${DIR}/cmusphinx-fr-5.2" "${acoustic_output}"
 
 #------------------------------------------------------------------------------
 # G2P
