079155856f4e829d7295a59314b680a05c497074
Michael Hansen <michael.hansen.24@us.af.mil>
2019-05-28 16:33:13 -0400

    First pass of Python-based file downloading

--- rhasspy/core.py
+++ rhasspy/core.py
@@ -2,6 +2,13 @@ import os
 import sys
 import logging
 import subprocess
+import shutil
+import tempfile
+import urllib.request
+import platform
+import gzip
+from collections import defaultdict
+import concurrent.futures
 from typing import List, Dict, Optional, Any, Callable, Tuple, Union
 
 import pydash
@@ -312,43 +319,221 @@ class RhasspyCore:
     # -------------------------------------------------------------------------
 
     def check_profile(self) -> bool:
-        """Returns (True, output) if the profile has all necessary files downloaded."""
-        check_path = self.profile.read_path("check-profile.sh")
-        assert os.path.exists(check_path), "Missing profile check script"
+        """Returns True if the profile has all necessary files downloaded."""
+        output_dir = self.profile.write_path()
+        downloaded = True
 
-        check_cmd = ["bash", check_path, self.profile.write_path()]
+        # Load configuration
+        conditions = self.profile.get("download.conditions", {})
 
-        # Only check embeddings if flair is being used
-        if self.profile.get("intent.system", "") != "flair":
-            check_cmd.append("--no-flair")
+        # Check conditions
+        for setting_name in conditions:
+            real_value = self.profile.get(setting_name, None)
 
-        self._logger.debug(check_cmd)
+            # Compare setting values
+            for setting_value, files_dict in conditions[setting_name].items():
+                compare_func = lambda v1, v2: v1 == v2
 
-        try:
-            subprocess.run(check_cmd, stderr=sys.stderr, stdout=sys.stderr, check=True)
-        except subprocess.CalledProcessError as e:
-            return False
+                if compare_func(setting_value, real_value):
+                    # Check if file needs to be downloaded
+                    for dest_name in files_dict:
+                        dest_path = os.path.join(output_dir, dest_name)
+                        if not os.path.exists(dest_path):
+                            self._logger.debug(
+                                f"Missing {dest_path} because of {setting_name} {setting_value}"
+                            )
+                            downloaded = False
 
-        return True
+        return downloaded
+
+    def _get_compare_func(self, value: str):
+        """Use mini-language to allow for profile setting value comparison."""
+        if value.startswith(">="):
+            value = float(value[2:])
+            return lambda v: v >= value
+        elif value.startswith("<="):
+            value = float(value[2:])
+            return lambda v: v <= value
+        elif value.startswith(">"):
+            value = float(value[1:])
+            return lambda v: v > value
+        elif value.startswith("<"):
+            value = float(value[1:])
+            return lambda v: v < value
+        elif value.startswith("!"):
+            return lambda v: v != value
+        else:
+            return lambda v: v == value
 
     # -------------------------------------------------------------------------
 
     def download_profile(self, delete=False) -> None:
-        """Downloads all necessary profile files from Github."""
-        download_script = os.path.abspath(self.profile.read_path("download-profile.sh"))
-        assert os.path.exists(download_script), "Profile download script is missing."
-        download_cmd = ["bash", download_script, self.profile.write_path()]
+        """Downloads all necessary profile files from the internet and extracts them."""
+        output_dir = self.profile.write_path()
+        download_dir = self.profile.write_path(
+            self.profile.get("download.cache_dir", "download")
+        )
 
-        if delete:
-            download_cmd.append("--delete")
+        if delete and os.path.exists(download_dir):
+            self._logger.debug(f"Deleting download cache at {download_dir}")
+            shutil.rmtree(download_dir)
 
-        # Only download embeddings if flair is being used
-        if self.profile.get("intent.system", "") != "flair":
-            download_cmd.append("--no-flair")
+        os.makedirs(download_dir, exist_ok=True)
 
-        self._logger.debug(download_cmd)
+        # Load configuration
+        conditions = self.profile.get("download.conditions", {})
+        all_files = self.profile.get("download.files", {})
+        files_to_copy = {}
+        files_to_extract = defaultdict(list)
+        files_to_download = set()
 
-        subprocess.run(download_cmd, stderr=sys.stderr, stdout=sys.stderr, check=True)
+        def download_file(url, filename):
+            try:
+                self._logger.debug(f"Downloading {url} to {filename}")
+                urllib.request.urlretrieve(url, filename)
+            except Exception as e:
+                self._logger.exception(url)
+
+        # Check conditions
+        machine_type = platform.machine()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for setting_name in conditions:
+                real_value = self.profile.get(setting_name, None)
+
+                # Compare setting values
+                for setting_value, files_dict in conditions[setting_name].items():
+                    compare_func = self._get_compare_func(setting_value)
+
+                    if compare_func(real_value):
+                        # Check if file needs to be downloaded
+                        for dest_name, src_name in files_dict.items():
+                            dest_path = os.path.join(output_dir, dest_name)
+                            if ":" in src_name:
+                                # File is an archive
+                                src_name, src_extract = src_name.split(":", maxsplit=1)
+                                src_path = os.path.join(download_dir, src_name)
+                                files_to_extract[src_path].append(
+                                    (dest_path, src_extract)
+                                )
+                            else:
+                                # Just a regular file
+                                src_path = os.path.join(download_dir, src_name)
+                                files_to_copy[src_path] = dest_path
+
+                            # Get download/cache info for file
+                            src_info = all_files.get(src_name, None)
+                            if src_info is None:
+                                self._logger.error(
+                                    f"No entry for download file {src_name}"
+                                )
+                                continue
+
+                            if not src_info.get("cache", True):
+                                # File will be downloaded in-place
+                                files_to_copy.pop(src_path)
+                                src_path = dest_path
+
+                            # Check if file is already in cache
+                            if os.path.exists(src_path):
+                                self._logger.debug(
+                                    f"Using cached {src_path} for {dest_name}"
+                                )
+                            else:
+                                # File needs to be downloaded
+                                src_url = src_info.get("url", None)
+                                if src_url is None:
+                                    # Try with machine type
+                                    if machine_type in src_info:
+                                        src_url = src_info[machine_type]["url"]
+                                    else:
+                                        self._logger.error(
+                                            f"No entry for download file {src_name} with machine type {machine_type}"
+                                        )
+                                        continue
+
+                                # Schedule file for download
+                                if not src_url in files_to_download:
+                                    executor.submit(download_file, src_url, src_path)
+                                    files_to_download.add(src_url)
+
+        # Copy files
+        for src_path, dest_path in files_to_copy.items():
+            # Remove existing file/directory
+            if os.path.isdir(dest_path):
+                self._logger.debug(f"Removing {dest_path}")
+                shutil.rmtree(dest_path)
+            elif os.path.isfile(dest_path):
+                self._logger.debug(f"Removing {dest_path}")
+                os.unlink(dest_path)
+
+            # Create necessary directories
+            os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+            # Copy file/directory as is
+            self._logger.debug(f"Copying {src_path} to {dest_path}")
+            if os.path.isdir(src_path):
+                shutil.copytree(src_path, dest_path)
+            else:
+                shutil.copy2(src_path, dest_path)
+
+        # Extract/install files
+        unpack_extensions = [
+            ext for fmt in shutil.get_unpack_formats() for ext in fmt[1]
+        ]
+
+        for src_path, extract_paths in files_to_extract.items():
+            # Check if the file extension will be understood by shutil.unpack_archive
+            known_format = False
+            for ext in unpack_extensions:
+                if src_path.endswith(ext):
+                    known_format = True
+
+            unpack = lambda temp_dir: shutil.unpack_archive(src_path, temp_dir)
+            if not known_format:
+                # Handle special archives
+                if src_path.endswith(".gz"):
+                    # Single file compressed with gzip
+                    def unpack_gz(temp_dir):
+                        # Strip off .gz and put relative to temporary directory
+                        temp_file_path = os.path.join(
+                            temp_dir, os.path.split(src_path[:-3])[1]
+                        )
+
+                        # Decompress single file
+                        with open(src_path, "rb") as src_file:
+                            with open(temp_file_path, "wb") as temp_file:
+                                temp_file.write(gzip.decompress(src_file.read()))
+
+                    unpack = unpack_gz
+                else:
+                    # Very bad situation
+                    self._logger.warning(
+                        f"Unknown archive extension {src_path}. This is probably going to fail."
+                    )
+
+            # Cached file is an archive. Unpack first.
+            with tempfile.TemporaryDirectory() as temp_dir:
+                unpack(temp_dir)
+
+                for dest_path, src_extract in extract_paths:
+                    # Remove existing file/directory
+                    if os.path.isdir(dest_path):
+                        self._logger.debug(f"Removing {dest_path}")
+                        shutil.rmtree(dest_path)
+                    elif os.path.isfile(dest_path):
+                        self._logger.debug(f"Removing {dest_path}")
+                        os.unlink(dest_path)
+
+                    # Create necessary directories
+                    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+                    # Copy specific file/directory
+                    extract_path = os.path.join(temp_dir, src_extract)
+                    self._logger.debug(f"Copying {extract_path} to {dest_path}")
+                    if os.path.isdir(extract_path):
+                        shutil.copytree(extract_path, dest_path)
+                    else:
+                        shutil.copy2(extract_path, dest_path)
 
     # -------------------------------------------------------------------------
 
