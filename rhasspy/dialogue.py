d8d64865082edf884c887a2490dfbf8213999ec6
Koen Vervloesem <koen@vervloesem.eu>
2019-12-09 21:36:33 +0100

    Import typing.List

--- rhasspy/dialogue.py
+++ rhasspy/dialogue.py
@@ -4,7 +4,7 @@ import os
 import sys
 from datetime import timedelta
 from pathlib import Path
-from typing import Any, Dict, Optional, Type
+from typing import Any, Dict, List, Optional, Type
 
 import pywrapfst as fst
 
