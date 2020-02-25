e5e8892d5103841f63db0715103bc4e4ef3cd1e4
Daniele Ricci <daniele@casaricci.it>
2020-02-25 19:53:26 +0100

    Fix Rasa tag synonyms
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- rhasspy/intent_train.py
+++ rhasspy/intent_train.py
@@ -197,7 +197,11 @@ class RasaIntentTrainer(RhasspyActor):
                         if entity and (raw_index >= entity["raw_end"]):
                             # Finish current entity
                             last_token = entity_tokens[-1]
-                            entity_tokens[-1] = f"{last_token}]({entity['entity']})"
+                            if entity['value'] != entity['raw_value']:
+                                synonym = f":{entity['value']}"
+                            else:
+                                synonym = ""
+                            entity_tokens[-1] = f"{last_token}]({entity['entity']}{synonym})"
                             sentence_tokens.extend(entity_tokens)
                             entity = None
                             entity_tokens = []
@@ -222,7 +226,11 @@ class RasaIntentTrainer(RhasspyActor):
                     if entity:
                         # Finish final entity
                         last_token = entity_tokens[-1]
-                        entity_tokens[-1] = f"{last_token}]({entity['entity']})"
+                        if entity['value'] != entity['raw_value']:
+                            synonym = f":{entity['value']}"
+                        else:
+                            synonym = ""
+                        entity_tokens[-1] = f"{last_token}]({entity['entity']}{synonym})"
                         sentence_tokens.extend(entity_tokens)
 
                     # Print single example
